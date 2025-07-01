a2383caa5c1dc9c6146d0676d7784c9f8ab174f1
Lainero <7Lainero7@gmail.com>
2025-07-01 15:32:41 +0500

    fixing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import BookPage from './pages/BookPage'
 import SettingsPage from './pages/SettingsPage'
+import { BookPageProvider } from './contexts/BookPageContext'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/book/:id" element={<BookPage />} />
-      <Route path="/settings" element={<SettingsPage />} />
+      <Route
+        path="/settings"
+        element={
+          <BookPageProvider>
+            <SettingsPage />
+          </BookPageProvider>
+        }
+      />
     </Routes>
   )
 }
