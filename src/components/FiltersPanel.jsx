4640fdfe589adf751d7b52bda6c390ab150f990f
Lainero <7Lainero7@gmail.com>
2025-07-01 14:08:08 +0500

    added FiltersPanel.jsx

--- src/components/FiltersPanel.jsx
+++ src/components/FiltersPanel.jsx
@@ -6,7 +6,6 @@ const FiltersPanel = () => {
     <div className="filters-panel">
       <h3>Фильтры</h3>
       <AuthorFilter options={['Mark Tielens Thomas', 'David Flanagan']} />
-      {/* Можно добавить RangeSlider и ToggleFavorites */}
       <ResetFiltersButton />
     </div>
   )
